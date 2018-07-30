d7f4aece2c264a1088e3bc6a42500ba08eea3072
student online <eliel.badon@gmail.com>
2018-07-30 23:10:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function takeANumber(linenum, name){
   for (var i = 0; i < linenum.length; i++){
-  katzDeliLine.push("Welcome," + name[1] + "You are number" + linenum[1] + "in line.")
+  katzDeliLine.push("Welcome," + name[i] + "You are number" + linenum[i] + "in line.")
   }
   return katzDeliLine;
 }
