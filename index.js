fdfc0911240fd8bcbf262e9aa5b490d2fcad67f5
student online <eliel.badon@gmail.com>
2018-07-31 00:04:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,13 @@ function nowServing(katzDeli){
   return (`Currently serving ${katzDeli.shift()}.`);
 }
 
-function currentLine(katzDeli){
-  
+	var line = [];
+ function currentLine(katzDeli) {
+  for (let i = 0; i < katzDeli.length; i++) {
+    line.push(` `+[i+1]+`. `  + katzDeli[i])
+  }
+  if (katzDeli.length === 0) {
+    return "The line is currently empty.";
+  } else
+  return(`The line is currently:` + line);
 }
-
